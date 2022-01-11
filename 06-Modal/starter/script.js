5c69311b3b6e74854006e74c833e279050a13e2d
rebecatschiedel <rebecatcmg@gmail.com>
2022-01-11 00:48:56 -0800

    add functionality: close modal clicking outside it

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,17 +1,22 @@
 'use strict';
 
+// Select all elements from DOM
 const buttons = document.querySelectorAll('.show-modal');
 const buttonCloseModal = document.querySelector('.close-modal');
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 
+// Show and hide the modal by toggling the class
 const handleClick = function () {
   modal.classList.toggle('hidden');
   overlay.classList.toggle('hidden');
 };
 
+// Open modal when click any of the buttons
 buttons.forEach(button => button.addEventListener('click', handleClick));
 
+// Close modal when click the button on the modal
 buttonCloseModal.addEventListener('click', handleClick);
 
 // Close the modal when click outside it
+overlay.addEventListener('click', handleClick);
