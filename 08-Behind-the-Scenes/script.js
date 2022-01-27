8490c84c80249000a8bd6e49f922c033c4bd41c0
rebecatschiedel <rebecatcmg@gmail.com>
2022-01-26 21:03:28 -0800

    Javascript behind the scenes - this keyword

--- 08-Behind-the-Scenes/script.js
+++ 08-Behind-the-Scenes/script.js
@@ -40,3 +40,44 @@ function calcAge(birthYear) {
 
 const firstName = 'Zelda';
 console.log(calcAge(2016));
+
+// This keyword
+
+console.log('this', this);
+
+const calcAge1 = function (birthYear) {
+  console.log(2037 - birthYear);
+  console.log(this);
+};
+
+calcAge1(1991);
+
+// Lexical
+const calcAge2 = birthYear => {
+  console.log(2037 - birthYear);
+  // This will point to the parent scope (window, in this case)
+  console.log(this);
+};
+
+calcAge2(1991);
+
+// Method
+const jonas = {
+  year: 1991,
+  calcAge3: function () {
+    console.log('this inside a method', this);
+    console.log(2037 - this.year);
+  },
+};
+jonas.calcAge3();
+
+const matilda = {
+  year: 2017,
+};
+
+matilda.calcAge3 = jonas.calcAge3;
+matilda.calcAge3();
+
+const f = jonas.calcAge3;
+
+f();
