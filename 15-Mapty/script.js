954dbeb62ae5db6d54d375d3718519454d4658ae
rebecatschiedel <rebecatcmg@gmail.com>
2022-05-29 10:28:17 -0700

    Refactoring for Project Architecture - Classes

--- 15-Mapty/script.js
+++ 15-Mapty/script.js
@@ -11,6 +11,55 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+class Workout {
+  //Public fields
+  date = new Date();
+  id = (Date.now() + '').slice(-10);
+
+  constructor(coords, distance, duration) {
+    this.coords = coords; //[lat,lng]
+    this.distance = distance; //Km
+    this.duration = duration; //min
+  }
+}
+
+class Running extends Workout {
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+
+    this.calcPace();
+  }
+
+  calcPace() {
+    //min/km
+    this.pace = this.duration / this.distance;
+    return this.pace;
+  }
+}
+class Cycling extends Workout {
+  constructor(coords, distance, duration, elGain) {
+    super(coords, distance, duration);
+    this.elGain = elGain;
+
+    this.calcSpeed();
+  }
+
+  calcSpeed() {
+    //km/h
+    this.speed = this.distance / (this.duration / 60);
+    return this.speed;
+  }
+}
+
+const run1 = new Running([39, -12], 5.2, 24, 178);
+const cycling1 = new Cycling([39, -11], 27, 95, 528);
+
+console.log(run1, cycling1);
+
+//////////////////////////
+// APLICATION ARCHITECTURE
+
 class App {
   #map;
   #mapEvent;
